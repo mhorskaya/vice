d0111fed9a5fe131c75a5195f07a05c6adaf2251
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2021-04-26 14:09:12 +0300

    Some lexer clean-up

--- Vice/vc/CodeAnalysis/Lexer.cs
+++ Vice/vc/CodeAnalysis/Lexer.cs
@@ -33,12 +33,8 @@ namespace Vice.CodeAnalysis
             _position++;
         }
 
-        public SyntaxToken NextToken()
+        public SyntaxToken Lex()
         {
-            // <numbers>
-            // + - * / ( )
-            // <whitespace>
-
             if (_position >= _text.Length)
             {
                 return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
@@ -77,29 +73,25 @@ namespace Vice.CodeAnalysis
                 return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
             }
 
-            if (Current == '+')
+            switch (Current)
             {
-                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
-            }
-            else if (Current == '-')
-            {
-                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
-            }
-            else if (Current == '*')
-            {
-                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
-            }
-            else if (Current == '/')
-            {
-                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
-            }
-            else if (Current == '(')
-            {
-                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
-            }
-            else if (Current == ')')
-            {
-                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+                case '+':
+                    return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+
+                case '-':
+                    return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+
+                case '*':
+                    return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+
+                case '/':
+                    return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+
+                case '(':
+                    return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+
+                case ')':
+                    return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
             }
 
             _diagnostics.Add($"ERROR: bad character input: '{Current}'");
