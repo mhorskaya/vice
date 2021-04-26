ed27fa4333329fcb1a1908d20f42d9162e48f803
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2021-04-26 19:46:35 +0300

    Replace switch with operator lookup

--- Vice/vc/CodeAnalysis/Evaluator.cs
+++ Vice/vc/CodeAnalysis/Evaluator.cs
@@ -28,7 +28,7 @@ namespace Vice.CodeAnalysis
             {
                 var operand = EvaluateExpression(u.Operand);
 
-                switch (u.OperatorKind)
+                switch (u.Op.Kind)
                 {
                     case BoundUnaryOperatorKind.Identity:
                         return (int)operand;
@@ -40,7 +40,7 @@ namespace Vice.CodeAnalysis
                         return !(bool)operand;
 
                     default:
-                        throw new Exception($"Unexpected unary operator {u.OperatorKind}");
+                        throw new Exception($"Unexpected unary operator {u.Op}");
                 }
             }
 
@@ -49,7 +49,7 @@ namespace Vice.CodeAnalysis
                 var left = EvaluateExpression(b.Left);
                 var right = EvaluateExpression(b.Right);
 
-                switch (b.OperatorKind)
+                switch (b.Op.Kind)
                 {
                     case BoundBinaryOperatorKind.Addition:
                         return (int)left + (int)right;
@@ -70,7 +70,7 @@ namespace Vice.CodeAnalysis
                         return (bool)left || (bool)right;
 
                     default:
-                        throw new Exception($"Unexpected binary operator {b.OperatorKind}");
+                        throw new Exception($"Unexpected binary operator {b.Op}");
                 }
             }
 
