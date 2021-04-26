a55b29a369f75e6e07cf8ff409814d57a9636ef2
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2021-04-26 14:29:46 +0300

    Move precedences to SyntaxFacts

--- Vice/vc/CodeAnalysis/Parser.cs
+++ Vice/vc/CodeAnalysis/Parser.cs
@@ -2,6 +2,26 @@
 
 namespace Vice.CodeAnalysis
 {
+    internal static class SyntaxFacts
+    {
+        public static int GetBinaryOperatorPrecedence(this SyntaxKind kind)
+        {
+            switch (kind)
+            {
+                case SyntaxKind.StarToken:
+                case SyntaxKind.SlashToken:
+                    return 2;
+
+                case SyntaxKind.PlusToken:
+                case SyntaxKind.MinusToken:
+                    return 1;
+
+                default:
+                    return 0;
+            }
+        }
+    }
+
     internal sealed class Parser
     {
         private readonly SyntaxToken[] _tokens;
@@ -76,7 +96,7 @@ namespace Vice.CodeAnalysis
 
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                 {
                     break;
@@ -88,23 +108,6 @@ namespace Vice.CodeAnalysis
             }
 
             return left;
-        }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-
-                default:
-                    return 0;
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
